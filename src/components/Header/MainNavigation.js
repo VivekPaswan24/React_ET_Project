65ca4e5b33c6d9748098669476f10f1bf610aaa7
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-03-20 22:35:15 +0530

    Added logout functionality

--- src/components/Header/MainNavigation.js
+++ src/components/Header/MainNavigation.js
@@ -1,12 +1,23 @@
+import { Button } from "react-bootstrap";
 import React from "react";
 
 import { Navbar,Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const MainNavigation = () => {
+  const token =localStorage.getItem('token')
+
+  const navigate=useNavigate();
+
+  const logoutHandler=()=>{
+    localStorage.removeItem('token')
+    navigate('/')
+  }
   return (
     <Navbar bg="light">
       <Container>
         <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
+        {token && <Button variant="outline-dark" onClick={logoutHandler} >Logout</Button>}
       </Container>
     </Navbar>
   );
