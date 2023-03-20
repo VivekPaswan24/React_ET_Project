be83d81f53f6e1cea83efaa08a1a4c7b7593f772
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-03-20 23:12:31 +0530

    Added Forgot Pasword Functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AuthPage from "./pages/Auth";
 import WelcomePage from "./pages/Welcome";
 import UpdatProfilePage, {loader as profileInfoLoader} from "./pages/UpdateProfile";
 import ErrorPage from "./pages/Error";
+import PasswordChangePage from "./pages/PasswordChange";
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
     {path:'/', element:<RootLayout/>, errorElement:<ErrorPage/>, children:[
       {index: true , element:<AuthPage/>},
       {path:'welcome',element:<WelcomePage/>},
-      {path:'updateProfile',element:<UpdatProfilePage/>,loader:profileInfoLoader}
+      {path:'updateProfile',element:<UpdatProfilePage/>,loader:profileInfoLoader},
+      {path:'forgot', element:<PasswordChangePage/>}
     ]}
   ])
   return (
--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -2,59 +2,62 @@ import React, { useRef, useState } from "react";
 import { Card, Button, Form, Container } from "react-bootstrap";
 import axios from "axios";
 
-import {useNavigate} from 'react-router-dom'
+import { useNavigate,Link } from "react-router-dom";
 
 const AuthForm = () => {
   const [error, setError] = useState(null);
-  const [isLogin,setIsLogin]=useState(true)
+  const [isLogin, setIsLogin] = useState(true);
 
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const confirmPasswordInputRef = useRef();
 
-  const switchAuthModeHandler=()=>{
-    setIsLogin((prevState)=>!prevState)
-  }
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
 
   const submitHandler = async (event) => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
-    if(!isLogin){
-        const enteredConfirmPassword = confirmPasswordInputRef.current.value;
-        if (enteredPassword === enteredConfirmPassword) {
-          try {
-            await axios.post(
-              "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD4GjTK67EiRG4F6h_wEsd1uUdZeP_sYvw",
-              {
-                email: enteredEmail,
-                password: enteredConfirmPassword,
-                returnSecureToken: true,
-              }
-            );
-            console.log("User has successfully sigend up.");
-          } catch (error) {
-            setError(error.response.data.error.message);
+    if (!isLogin) {
+      const enteredConfirmPassword = confirmPasswordInputRef.current.value;
+      if (enteredPassword === enteredConfirmPassword) {
+        try {
+          await axios.post(
+            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD4GjTK67EiRG4F6h_wEsd1uUdZeP_sYvw",
+            {
+              email: enteredEmail,
+              password: enteredConfirmPassword,
+              returnSecureToken: true,
+            }
+          );
+          console.log("User has successfully sigend up.");
+        } catch (error) {
+          setError(error.response.data.error.message);
+        }
+      } else {
+        setError("Password is not matched");
+      }
+    } else {
+      try {
+        const response = await axios.post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD4GjTK67EiRG4F6h_wEsd1uUdZeP_sYvw",
+          {
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
           }
-        } else {
-          setError("Password is not matched");
-        }
-      }else{
-        try{
-            const response=await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD4GjTK67EiRG4F6h_wEsd1uUdZeP_sYvw',{
-                email:enteredEmail,
-                password:enteredPassword,
-                returnSecureToken:true
-            })
-            localStorage.setItem('token',response.data.idToken)
-            navigate('/welcome')
-        }catch(error){
-            setError(error.response.data.error.message)
-        }
+        );
+        localStorage.setItem("token", response.data.idToken);
+        navigate("/welcome");
+      } catch (error) {
+        setError(error.response.data.error.message);
       }
     }
+  };
 
   return (
     <React.Fragment>
@@ -67,7 +70,7 @@ const AuthForm = () => {
       >
         <Card.Body>
           <Card.Title style={{ color: "black", textAlign: "center" }}>
-            {!isLogin ? 'Sign Up':'Login'}
+            {!isLogin ? "Sign Up" : "Login"}
           </Card.Title>
           <Form onSubmit={submitHandler}>
             <Form.Group className="mb-4 mt-4">
@@ -81,25 +84,28 @@ const AuthForm = () => {
 
             <Form.Group className="mb-4">
               <Form.Control
-                type={!isLogin ? 'text':'password'}
+                type={!isLogin ? "text" : "password"}
                 placeholder="Password"
                 required
                 ref={passwordInputRef}
               />
             </Form.Group>
-            {!isLogin && <Form.Group className="mb-4">
-              <Form.Control
-                type="password"
-                placeholder="Confirm Password"
-                required
-                ref={confirmPasswordInputRef}
-              />
-            </Form.Group>}
+            {!isLogin && (
+              <Form.Group className="mb-4">
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  required
+                  ref={confirmPasswordInputRef}
+                />
+              </Form.Group>
+            )}
             <Button variant="primary" type="submit">
-              {isLogin ? 'Login' :'Sign Up'}
+              {isLogin ? "Login" : "Sign Up"}
             </Button>
           </Form>
         </Card.Body>
+        <Link to='/forgot' style={{textAlign:'center',textDecoration:'none'}}>Forgot password?</Link>
       </Card>
       <Container
         style={{
@@ -113,9 +119,15 @@ const AuthForm = () => {
         }}
       >
         {!error ? (
-            <button type="click" style={{ backgroundColor: "transparent", border: "none" }} onClick={switchAuthModeHandler}>
-              {!isLogin ? 'Have an account ? Login':"Don't have an account? Sign up" }
-            </button>
+          <button
+            type="click"
+            style={{ backgroundColor: "transparent", border: "none" }}
+            onClick={switchAuthModeHandler}
+          >
+            {!isLogin
+              ? "Have an account ? Login"
+              : "Don't have an account? Sign up"}
+          </button>
         ) : (
           <span>{error}</span>
         )}
